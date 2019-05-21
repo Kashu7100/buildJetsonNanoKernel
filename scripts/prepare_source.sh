bc4d0fcc83841c0ab2ea1eea7b1ca97f7c62e990
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-05-20 19:59:30 -0500

    Update prepare_source.sh

--- scripts/prepare_source.sh
+++ scripts/prepare_source.sh
@@ -6,7 +6,7 @@ cd /usr/src
 wget -N https://developer.nvidia.com/embedded/dlc/l4t-sources-32-1-jetson-nano
 sudo tar -xvf public_sources.tbz2 public_release/kernel_src.tbz2
 tar -xvf public_sources/kernel_src.tbz2
-rm -r public_release
+rm -r public_sources
 cd kernel/kernel-4.9
 #get the new system configuration
 zcat /proc/config.gz > .config
